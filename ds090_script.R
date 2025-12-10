3a2c5879abaee198d1e375d48855c4050a305f58
tb25aay <tb25aay@herts.ac.uk>
2025-12-11 02:04:06 +0530

    update visualization code Histogram

--- ds090_script.R
+++ ds090_script.R
@@ -1,64 +1,40 @@
+#install.packages("tidyverse")
+library(tidyverse)
 
-# Load dataset
-df <- read.csv("tidy_format_co2_emission_dataset.csv")
+# Load dataset and keep original names
+CO2 <- read.csv("tidy_format_co2_emission_dataset.csv", check.names = FALSE)
 
-# Check the structure
-str(df)
+# Rename to simple names
+colnames(CO2) <- c("country", "year", "CO2")
 
-#print few rows
-head(df)
+# Convert CO2 to numeric
+CO2$CO2 <- as.numeric(CO2$CO2)
 
-#rename CO2Emission Rate..mt.
-names(df)[3] <- "CO2"
+#remove rows with null CO2
+CO2 <- CO2 %>% drop_na(CO2)
 
-#print column names
-colnames(df)
+# Filter two years
+CO2_1990 <- CO2 %>%
+  filter(year == 1990) %>%
+  select(country, CO2_1990 = CO2)
 
-# Convert CO₂ emissions column into numeric (important!)
-df$CO2 <- as.numeric(df$CO2)
+CO2_2021 <- CO2 %>%
+  filter(year == 2021) %>%
+  select(country, CO2_2021 = CO2)
 
-#Data set col Min,max,quartile,Mean,median,mode
-summary(df)
+# Merge & compute differences
+paired <- inner_join(CO2_1990, CO2_2021, by = "country") %>%
+  mutate(diff = CO2_2021 - CO2_1990)
 
-# Count how many countries exist in each year
-table(df$Year)
-
-# Filter the two years we want
-df1990 <- df[df$Year == 1990, ]
-df2021 <- df[df$Year == 2021, ]
-
-# Show how many rows we have (to justify sample size)
-nrow(df1990)
-nrow(df2021)
-
-# Create two groups (1990 vs 2021)
-
-df1990$Group <- "1990"
-df2021$Group <- "2021"
-combined <- rbind(df1990, df2021)
+# BOXPLOT
 
 
-# Histogram → helps justify use of t-test vs nonparametric test
-hist(combined$CO2,
-     breaks = 30,
-     main = "Distribution of CO₂ Emissions",
-     xlab = "CO₂ Emission Rate (mt)",
+# HISTOGRAM
+png("hist_diff_CO2.png")
+hist(paired$diff, breaks=20,
+     main="Histogram of CO2 Emission Differences (2021 - 1990)",
+     xlab="Change in CO2 Emission (metric tonnes)",
+     ylab="Frequency",
      col = "lightblue")
-
-
-# Boxplot → visual comparison of the two years
-
-boxplot(CO2 ~ Group,
-        data = combined,
-        col = c("skyblue", "orange"),
-        main = "CO₂ Emissions in 1990 vs 2021",
-        xlab = "Year",
-        ylab = "CO₂ Emission Rate (mt)")
-
-
-# First, try the t-test
-t.test(CO2 ~ Group, data = combined)
-
-# If histogram shows data is NOT normal, use non-parametric test:
-wilcox.test(CO2 ~ Group, data = combined)
+dev.off()
 
